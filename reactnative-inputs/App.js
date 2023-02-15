bb80e28393511c82d790ca08a8fc3b6b8195f2e6
Daniel Ruiz <108552382+RuizDann@users.noreply.github.com>
2023-02-15 12:43:37 -0600

    update

--- reactnative-inputs/App.js
+++ reactnative-inputs/App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
