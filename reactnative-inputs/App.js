696878df607dba7f0508afb93610bb4764913b15
Daniel Ruiz <108552382+RuizDann@users.noreply.github.com>
2023-02-17 12:21:40 -0600

    update

--- reactnative-inputs/App.js
+++ reactnative-inputs/App.js
@@ -9,16 +9,16 @@ function HomeScreen({ navigation }) {
       <Text>Home Screen</Text>
       <Button
         title="Create Match"
-        onPress={() => navigation.navigate('CreateMatch')}
+        onPress={() => navigation.navigate('Enter Wrestler #1 info')}
       />
     </View>
   );
 }
 
-function CreateMatchScreen() {
+function Wrestler1Screen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Create Match Screen</Text>
+      <Text>Wrestler 1 Screen</Text>
     </View>
   );
 }
@@ -29,8 +29,15 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CreateMatch" component={CreateMatchScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} 
+        options={{
+            title: 'My home',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+          }}/>
+        <Stack.Screen name="Enter Wrestler #1 info" component={Wrestler1Screen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
